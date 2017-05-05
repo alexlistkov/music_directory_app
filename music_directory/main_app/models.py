f984d502dad3e582cf372c548577fa48d08713ae
Alexander <casparados@yandex.ru>
2017-05-05 19:43:17 +0300

    some model and template updates, v0.1.2

--- music_directory/main_app/models.py
+++ music_directory/main_app/models.py
@@ -6,9 +6,10 @@ from django.db import models
 class Music(models.Model):
     title = models.CharField(max_length = 100)
     artist = models.CharField(max_length = 100)
+    genre = models.CharField(max_length = 100)
     album = models.CharField(max_length = 100)
     year = models.IntegerField(default = 0)
-    cover = models.CharField(max_length = 100)
+    cover = models.CharField(max_length = 100, default = 'http://icons.iconarchive.com/icons/iconsmind/outline/512/CD-icon.png')
 
     def __str__(self):
         return self.title
