b6181a3c11ecfc974d02294178b3178505b946de
Alexander <casparados@yandex.ru>
2017-05-07 00:34:22 +0300

    added aunthetication, registration, htmls, some fixes, v0.3.5

--- music_directory/main_app/urls.py
+++ music_directory/main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^([0-9]+)/$', views.detail, name = 'detail'),
     url(r'^post_url/$', views.post_music, name = 'post_music'),
     url(r'^user/(\w+)/$', views.profile, name = 'profile'),
+    url(r'^login/$', views.login_view, name = 'login'),
+    url(r'^logout/$', views.logout_view, name = 'logout'),
+    url(r'^register/$', views.register, name = 'register'),
 ]
--- music_directory/main_app/views.py
+++ music_directory/main_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Music
-from .forms import MusicForm
+from .forms import MusicForm, LoginForm
 
 # Create your views here.
 
@@ -27,3 +29,36 @@ def profile(request, username):
     user = User.objects.get(username = username)
     musics = Music.objects.filter(user = user)
     return render(request, 'profile.html', {'username': username, 'musics': musics})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+                else:
+                    print("The account has been disabled!")
+            else:
+                print("The username and password were incorrect.")
+    else:
+        form = LoginForm()
+        return render(request, 'login.html', { 'form': form })
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/login/')
+    else:
+        form = UserCreationForm()
+        return render(request, 'registration.html', { 'form': form })
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
