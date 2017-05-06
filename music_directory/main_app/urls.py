fbc249de74248d31ee112ed6c11239f7713dfea5
Alexander <casparados@yandex.ru>
2017-05-06 03:08:05 +0300

    added details, added forms, some fixes, v0.2.0

--- music_directory/main_app/urls.py
+++ music_directory/main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
+    url(r'^([0-9]+)/$', views.detail, name = 'detail'),
+    url(r'^post_url/$', views.post_music, name = 'post_music'),
 ]
--- music_directory/main_app/views.py
+++ music_directory/main_app/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Music
+from .forms import MusicForm
 
 # Create your views here.
 
 def index(request):
     musics = Music.objects.all()
-    return render(request, 'index.html', {'musics': musics})
+    form = MusicForm
+    return render(request, 'index.html', { 'musics': musics, 'form': form })
+
+def detail(request, music_id):
+    music = Music.objects.get(id = music_id)
+    return render(request, 'details.html', { 'music': music })
+
+def post_music(request):
+    form = MusicForm(request.POST)
+    if form.is_valid():
+        form.save(commit = True)
+    return HttpResponseRedirect('/')
